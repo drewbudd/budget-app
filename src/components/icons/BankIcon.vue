e19d0ec094e847c9437dd5a0be69b4b437023e66
Andrew Almaguer <andrew@almaguer.dev>
2023-12-24 18:06:31 +0100

    Add TransactionTypeToggle and icons used for it

---
+++ src/components/icons/BankIcon.vue
@@ -0,0 +1,27 @@
+<template>
+  <IconBase collection="mdi" icon="bank" :size="size" :color="color" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import IconBase from './IconBase.vue';
+
+export default defineComponent({
+  name: 'BankIcon',
+  components: {
+    IconBase
+  },
+  props: {
+    size: {
+      type: Number,
+      required: false,
+      default: () => undefined
+    },
+    color: {
+      type: String,
+      required: false,
+      default: () => undefined
+    },
+  },
+})
+</script>
