c4bd5d0c4f1329b97110671c40bd9f08890caf8f
Andrew Almaguer <andrew@almaguer.dev>
2024-01-01 17:57:05 +0100

    Add download button and store getter for transactions as CSV

---
+++ src/components/icons/FileDownloadIcon.vue
@@ -0,0 +1,27 @@
+<template>
+  <IconBase collection="mdi" icon="file-download" :size="size" :color="color" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import IconBase from './IconBase.vue'
+
+export default defineComponent({
+  name: 'FileDownloadIcon',
+  components: {
+    IconBase,
+  },
+  props: {
+    size: {
+      type: Number,
+      required: false,
+      default: () => undefined,
+    },
+    color: {
+      type: String,
+      required: false,
+      default: () => undefined,
+    },
+  },
+})
+</script>
