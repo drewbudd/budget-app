86ccd78dab78cd5f7dacd84a49563ee44fab0099
Andrew Almaguer <andrew@almaguer.dev>
2024-01-03 08:45:06 +0100

    Implement a rough import functionality

--- src/components/data/UploadTransactions.vue
+++ src/components/data/UploadTransactions.vue
@@ -4,10 +4,16 @@
     <dialog id="file_upload_modal" class="modal">
       <div class="modal-box">
         <form method="dialog">
-          <button class="btn btn-sm btn-ghost btn-circle absolute right-2 top-2">X</button>
+          <button class="btn btn-sm btn-ghost btn-circle absolute right-2 top-2" ref="closeButton">X</button>
         </form>
         <h3 class="font-bold text-lg">Transaction Import</h3>
-        <p class="py-4">UPLOAD</p>
+        <div class="flex flex-col my-2">
+          <input type="file" class="file-input file-input-bordered file-input-primary w-full max-w-lg" accept="text/csv"
+            @change="setFilePath" />
+          <div class="flex justify-end mt-2">
+            <SubmitButton class="grow-0" :disabled="submitButtonDisabled" @click="importTransactions" />
+          </div>
+        </div>
       </div>
     </dialog>
   </div>
@@ -17,10 +23,13 @@
 import { defineComponent } from 'vue'
 import { useTransactionsStore } from '@/stores/transactions'
 import FileUploadIcon from '../icons/FileUploadIcon.vue'
+import SubmitButton from '../button/SubmitButton.vue'
+import Papa from 'papaparse'
+import type { TransactionEntry } from '@/types/transaction/TransactionEntry'
 
 export default defineComponent({
   name: 'UploadTransactions',
-  components: { FileUploadIcon },
+  components: { FileUploadIcon, SubmitButton },
   props: {
     disabled: {
       type: Boolean,
@@ -28,14 +37,38 @@ export default defineComponent({
       default: () => false,
     },
   },
+  data() {
+    return {
+      file: undefined as File | undefined,
+    }
+  },
   setup() {
     const transactionsStore = useTransactionsStore()
     return { transactionsStore }
   },
   computed: {
-    downloadLink(): HTMLAnchorElement {
-      return this.$refs.downloadLink as HTMLAnchorElement
+    closeButton(): HTMLButtonElement {
+      return this.$refs.closeButton as HTMLButtonElement
+    },
+    submitButtonDisabled() {
+      return !this.file
     },
   },
+  methods: {
+    setFilePath(event: Event) {
+      this.file = (event.target as HTMLInputElement | null)?.files?.[0] ?? undefined
+    },
+    importTransactions() {
+      if (this.file === undefined) return
+      Papa.parse(this.file, {
+        header: true,
+        dynamicTyping: true,
+        complete: (results) => {
+          this.transactionsStore.setTransactions(results.data as TransactionEntry[])
+          this.closeButton.click()
+        }
+      })
+    }
+  }
 })
 </script>
